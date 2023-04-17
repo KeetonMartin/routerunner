999aa6f20e7472b462d4f4ebaef7a2f06bb2ec56
Keeton Martin <keeton.martin@gmail.com>
2023-04-17 01:03:02 -0400

    analytics 2

--- src/index.js
+++ src/index.js
@@ -4,13 +4,6 @@ import './index.css';
 import App from './App';
 import store from './app/store'
 import { Provider } from 'react-redux'
-import TagManager from 'react-gtm-module'
- 
-const tagManagerArgs = {
-    gtmId: 'GTM-TL5XF75'
-}
- 
-TagManager.initialize(tagManagerArgs)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
